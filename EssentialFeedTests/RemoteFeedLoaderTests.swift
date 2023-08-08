a45969dac23da01976cb71518aa630b354c5161d
amir soltani <amirsoltani686@gmail.com>
2023-08-08 23:58:44 +0330

    Improve test's names

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -6,35 +6,18 @@
 //
 
 import XCTest
-
-class RemoteFeedLoader {
-    var url: URL
-    var client: HTTPClient
-    
-    internal init(url: URL ,client: HTTPClient) {
-        self.client = client
-        self.url = url
-    }
-    
-    func load() {
-        client.get(from: url)
-    }
-}
-
-protocol HTTPClient {
-    func get(from url: URL)
-}
+@testable import EssentialFeed
 
 final class RemoteFeedLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
-        let (_, client) = makeSUT(client: HTTPClientSpy())
+        let (_, client) = makeSUT()
         
         XCTAssertNil(client.requestedURL)
     }
     
-    func test_load_requestDataFromURL() {
-        let (sut, client) = makeSUT(url: URL(string: "https://a-given-url.com")!, client: HTTPClientSpy())
+    func test_load_requestsDataFromURL() {
+        let (sut, client) = makeSUT(url: URL(string: "https://a-given-url.com")!)
         
         sut.load()
         
@@ -44,7 +27,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
     
     // MARK - Helpers
     
-    func makeSUT(url: URL = URL(string: "https://a-url.com")!, client: HTTPClient) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+    func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
         let client = HTTPClientSpy()
         let sut = RemoteFeedLoader(url: url, client: client)
         return (sut: sut, client: client)
--- Feed Feature/FeedLoader.swift
+++ Feed Feature/FeedLoader.swift
@@ -10,3 +10,21 @@ import Foundation
 protocol FeedLoader {
     func load(completion: @escaping () -> Result<[FeedItem], Error>)
 }
+
+class RemoteFeedLoader {
+    var url: URL
+    var client: HTTPClient
+    
+    internal init(url: URL ,client: HTTPClient) {
+        self.client = client
+        self.url = url
+    }
+    
+    func load() {
+        client.get(from: url)
+    }
+}
+
+protocol HTTPClient {
+    func get(from url: URL)
+}
