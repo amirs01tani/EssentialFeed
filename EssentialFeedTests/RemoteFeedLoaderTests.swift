aa82a3a5d38a6661e08c44dbfd283e521c7ec809
amir soltani <amirsoltani686@gmail.com>
2023-08-08 18:05:30 +0330

    Can get a url

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -8,15 +8,16 @@
 import XCTest
 
 class RemoteFeedLoader {
-    
+    var url: URL
     var client: HTTPClient
     
-    internal init(client: HTTPClient) {
+    internal init(url: URL ,client: HTTPClient) {
         self.client = client
+        self.url = url
     }
     
-    func load( ) {
-        client.get(from: URL(string: "https://a-url.com")!)
+    func load() {
+        client.get(from: url)
     }
 }
 
@@ -35,19 +36,21 @@ class HTTPClientSpy: HTTPClient {
 final class RemoteFeedLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
+        let url = URL(string: "https://a-given-url.com")!
         let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(client: client)
+        let sut = RemoteFeedLoader(url: url, client: client)
         
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestDataFromURL() {
+        let url = URL(string: "https://a-given-url.com")!
         let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(client: client)
+        let sut = RemoteFeedLoader(url: url, client: client)
         
         sut.load()
         
-        XCTAssertNotNil(client.requestedURL)
+        XCTAssertEqual(client.requestedURL, url)
         
     }
     
