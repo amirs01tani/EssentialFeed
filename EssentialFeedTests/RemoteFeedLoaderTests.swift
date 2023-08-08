0ff276c3562cf4090d1fa8ec5ee47712b6a9a2dd
amir soltani <amirsoltani686@gmail.com>
2023-08-08 14:49:44 +0330

    Implementing HTTPClient as a Singletone class

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -8,10 +8,14 @@
 import XCTest
 
 class RemoteFeedLoader {
-    
+    func load() {
+        HTTPClient.shared.requestedURL = URL(string: "https://a-url.com")
+    }
 }
 
 class HTTPClient {
+    static let shared = HTTPClient()
+    private init() {}
     var requestedURL: URL?
 }
 
@@ -19,9 +23,21 @@ final class RemoteFeedLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
         let sut = RemoteFeedLoader()
-        let client = HTTPClient()
+        let client = HTTPClient.shared
 
         XCTAssertNil(client.requestedURL)
     }
+    
+    func test_load_requestDataFromURL() {
+        let sut = RemoteFeedLoader()
+        let client = HTTPClient.shared
+        
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
+        
+    }
+    
+    
 
 }
