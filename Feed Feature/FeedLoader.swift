ce4b25c64e6e47f0dbe04ee3cc0a0449903c5673
amir soltani <amirsoltani686@gmail.com>
2023-08-09 23:57:05 +0330

    Separate HTTPClient related to a specific class called HTTPClient

--- Feed Feature/FeedLoader.swift
+++ Feed Feature/FeedLoader.swift
@@ -7,19 +7,6 @@
 
 import Foundation
 
-//protocol FeedLoader {
-//    func load(completion: @escaping () -> Result<[FeedItem], Error>)
-//}
-
-public enum HTTPClientResult {
-    case success(Data, HTTPURLResponse)
-    case failure(Error)
-}
-
-public protocol HTTPClient {
-    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
-}
-
 public class RemoteFeedLoader {
     
     private var url: URL
