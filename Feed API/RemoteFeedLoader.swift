777265469bed847a45e9ffdbff5600e02ce298de
amir soltani <amirsoltani686@gmail.com>
2023-08-10 17:21:45 +0330

    Replace remodefeedloader enum with feedloader result to remotefeedloader to confirm to feedloader protocol

---
+++ Feed API/RemoteFeedLoader.swift
@@ -0,0 +1,39 @@
+//
+//  FeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Amir on 8/7/23.
+//
+
+import Foundation
+
+public class RemoteFeedLoader {
+    
+    private var url: URL
+    private var client: HTTPClient
+    
+    public enum Error: Swift.Error {
+        case connectivity
+        case invalidData
+    }
+    
+    public typealias Result = LoadFeedResult<Error>
+    
+    public init(url: URL ,client: HTTPClient) {
+        self.client = client
+        self.url = url
+    }
+    
+    public func load(completion: @escaping (LoadFeedResult<Error>) -> Void) {
+        client.get(from: url, completion: { [weak self] result in
+            guard let _ = self else { return }
+            switch result {
+            case .success(let data, let response):
+                completion(FeedItemMapper.map(data, from: response))
+            case .failure:
+                completion(.failure(.connectivity))
+            }
+           
+        })
+    }
+}
--- Feed Feature/FeedLoader.swift
+++ Feed Feature/FeedLoader.swift
@@ -2,42 +2,18 @@
 //  FeedLoader.swift
 //  EssentialFeed
 //
-//  Created by Amir on 8/7/23.
+//  Created by Amir on 8/10/23.
 //
 
 import Foundation
 
-public class RemoteFeedLoader {
-    
-    private var url: URL
-    private var client: HTTPClient
-    
-    public enum Error: Swift.Error {
-        case connectivity
-        case invalidData
-    }
-    
-    public enum Result: Equatable {
-        
-        case success([FeedItem])
-        case failure(Error)
-    }
-    
-    public init(url: URL ,client: HTTPClient) {
-        self.client = client
-        self.url = url
-    }
-    
-    public func load(completion: @escaping (RemoteFeedLoader.Result) -> Void) {
-        client.get(from: url, completion: { [weak self] result in
-            guard let _ = self else { return }
-            switch result {
-            case .success(let data, let response):
-                completion(FeedItemMapper.map(data, from: response))
-            case .failure:
-                completion(.failure(.connectivity))
-            }
-           
-        })
-    }
+public enum LoadFeedResult<Error: Swift.Error> {
+    case success([FeedItem])
+    case failure(Error)
+}
+extension LoadFeedResult: Equatable where Error: Equatable {}
+
+protocol FeedLoader {
+    associatedtype Error: Swift.Error
+    func load (completion: @escaping (LoadFeedResult<Error>) -> Void)
 }
