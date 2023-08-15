37504c9c18ee7ed350d153ea49a60adc9e07e593
amir soltani <amirsoltani686@gmail.com>
2023-08-16 02:35:00 +0330

    Add save resukt type alias to protect our code from future changes

--- Feed Cache/LocalFeedLoader.swift
+++ Feed Cache/LocalFeedLoader.swift
@@ -7,17 +7,18 @@
 
 import Foundation
 
-public class LocalFeedLoader {
+final public class LocalFeedLoader {
     
     public var store: FeedStore
     private let currentDate: () -> Date
+    public typealias SaveResult = Error?
     
     public init(store: FeedStore, currentDate: @escaping () -> Date ) {
         self.store = store
         self.currentDate = currentDate
     }
     
-    public func save(_ items: [FeedItem], completion: @escaping (Error?) -> Void) {
+    public func save(_ items: [FeedItem], completion: @escaping (SaveResult) -> Void) {
         store.deleteCachedFeed(completion: { [weak self] error in
             guard let self = self else { return }
             if let cacheDeletionError = error {
@@ -28,7 +29,7 @@ public class LocalFeedLoader {
         })
     }
     
-    private func cache(_ items: [FeedItem], with completion: @escaping (Error?) -> Void) {
+    private func cache(_ items: [FeedItem], with completion: @escaping (SaveResult) -> Void) {
         store.insertItems(items, timeStamp: currentDate()) { [weak self] error in
             guard self != nil else { return }
             completion(error)
