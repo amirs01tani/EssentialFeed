a77a9b1a0c118cd082d7313580d01139eecfcdbe
amir soltani <amirsoltani686@gmail.com>
2023-08-17 20:27:52 +0330

    Delivers no images on seven days old cache

--- Feed Cache/LocalFeedLoader.swift
+++ Feed Cache/LocalFeedLoader.swift
@@ -38,19 +38,27 @@ final public class LocalFeedLoader {
     }
     
     public func load(with completion: @escaping (LoadResult) -> Void) {
-        store.retrieve { result in
+        store.retrieve { [unowned self] result in
             switch result {
             case let .failure(error):
                 completion(.failure(error))
                 
-            case let .found(feed, _):
+            case let .found(feed, timestamp) where self.validate(timestamp):
                 completion(.success(feed.toModels()))
                 
-            case .empty:
+            case .found, .empty:
                 completion(.success([]))
             }
         }
     }
+    
+    private func validate(_ timestamp: Date) -> Bool {
+             let calendar = Calendar(identifier: .gregorian)
+             guard let maxCacheAge = calendar.date(byAdding: .day, value: 7, to: timestamp) else {
+                 return false
+             }
+             return currentDate() < maxCacheAge
+         }
 }
 
 public extension Array where Element == FeedImage {
