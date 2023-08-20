b39145fe1564cfb0829aaab328dd53c030643266
amir soltani <amirsoltani686@gmail.com>
2023-08-20 21:13:01 +0330

    Separate Date extension into two extensions one for cache policy and one public helper

--- EssentialFeedTests/Feed Cache/Helpers/FeedCacheTestsHelpers.swift
+++ EssentialFeedTests/Feed Cache/Helpers/FeedCacheTestsHelpers.swift
@@ -19,7 +19,6 @@ func uniqueImageFeed() -> (models: [FeedImage], local: [LocalFeedImage]) {
 }
 
 extension Date {
-    
     func minusFeedCacheMaxAge() -> Date {
         return self.adding(days: -feedCacheMaxAgeInDays)
     }
@@ -28,10 +27,12 @@ extension Date {
         return 7
     }
     
-     func adding(days: Int) -> Date {
-         return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
-     }
+    private func adding(days: Int) -> Date {
+        return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
+    }
+}
 
+extension Date {
      func adding(seconds: TimeInterval) -> Date {
          return self + seconds
      }
